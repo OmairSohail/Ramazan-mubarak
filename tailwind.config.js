fcd9183cb075c02624d21a75888344cc37472464
OmairSohail <halakokhan1904@gmail.com>
2020-05-03 14:20:18 +0500

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'custom-black': '#01161B',
-        'primary':'#01161B',
-        'secondary':'#172720',
-        'golden':'#BD9756',
-        'dark-golden':'#946A2E',
+        
+        
       }
     },
   },
